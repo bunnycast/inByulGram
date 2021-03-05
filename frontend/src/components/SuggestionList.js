6c774b57a9291fd2518c70a2ba418b0e570ec871
bunnycast <berzzubunny@gmail.com>
2021-03-05 17:31:05 +0900

    frontend: useAxios hooks

--- frontend/src/components/SuggestionList.js
+++ frontend/src/components/SuggestionList.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, {useEffect, useState} from 'react';
 import {Card} from "antd";
 import Suggestion from "./Suggestion";
-import Axios from "axios";
-import { useAppContext } from "store.js";
+import useAxios from "axios-hooks";
+import {useAppContext} from "store.js";
 import './SuggestionList.scss'
 
 export default function SuggestionList ({ style }) {
@@ -10,27 +10,23 @@ export default function SuggestionList ({ style }) {
         store: { jwtToken }
     } = useAppContext();
 
-    const [userList, setUserList] = useState([]);
+    const headers = { Authorization: `JWT ${jwtToken}`};
 
-    useEffect(() => {
-        async function fetchUserList() {
-            const apiUrl = "http://localhost:8000/accounts/suggestions/";
-            const headers = { Authorization: `JWT ${ jwtToken }`}
-            try {
-               const { data } = await Axios.get(apiUrl, { headers });
-               setUserList(data);
-            }
-            catch (error) {
-                console.error(error);
-            }
-        }
-        fetchUserList();
-    },[]);
+    const [{ data: userList, loading, error }, refetch] = useAxios({
+        url: "http://localhost:8000/accounts/suggestions/",
+        headers
+    });
 
     return (
         <div style={style}>
+            {/*{loading && <div>...loading</div>}*/}
+            {/*{error && <div>an error occur by loading</div>}*/}
+
+            {/*<button onClick={() => refetch()}>Reload</button>*/}
+
             <Card title="Suggestions for you" size="small">
-                { userList.map(suggestionUser => (
+                { userList &&
+                userList.map(suggestionUser => (
                     <Suggestion
                         key ={suggestionUser.username}
                         suggestionUser={suggestionUser}
